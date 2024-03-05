d8544e6e0baa6a8f3b06da0d15c53c4d6f6c4cd5
SimraJabeen <simrajabeen16@gmail.com>
2024-03-06 01:15:56 +0500

    version 1

---
+++ main.ts
@@ -0,0 +1,27 @@
+// // variable Declaration
+// // console.log(myName);
+// let myName;
+//  myName  ='Simra';
+// // console.log(myName)
+// console.log(myName)
+// myName ="Jabeen";
+// console.log(myName);
+// const age ="19";
+// console.log(age);
+// age = '19'
+// console.log(age);
+// let response:boolean = true
+
+
+
+// let Fname:string = 'simra';
+// let Age:number;
+// Age =24
+// let Responses:boolean;
+// response = true
+// const Height:number =5.4
+// var student = 'simra'
+// var student = 'mumbai'
+
+let Name : string = "simra mumbai";
+console.log(Name);
